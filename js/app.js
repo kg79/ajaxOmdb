1d2b073fef4f5e62d80c67d917299acacfe68efe
Kevin Galvan <klgalvan79@gmail.com>
2018-03-01 17:43:32 -0600

    ajac hero

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-(function() {
+
+(function(){
   'use strict';
 
-  const movies = [];
+  let movies = [];
 
   const renderMovies = function() {
     $('#listings').empty();
 
     for (const movie of movies) {
+      console.log(movie);
       const $col = $('<div>').addClass('col s6');
       const $card = $('<div>').addClass('card hoverable');
       const $content = $('<div>').addClass('card-content center');
@@ -14,16 +16,16 @@
 
       $title.attr({
         'data-position': 'top',
-        'data-tooltip': movie.title
+        'data-tooltip': movie.Title
       });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+      $title.tooltip({ delay: 50 }).text(movie.Title);
 
       const $poster = $('<img>').addClass('poster');
 
       $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
+        src: movie.Poster,
+        alt: `${movie.Poster} Poster`
       });
 
       $content.append($title, $poster);
@@ -33,17 +35,17 @@
       const $plot = $('<a>');
 
       $plot.addClass('waves-effect waves-light btn modal-trigger');
-      $plot.attr('href', `#${movie.id}`);
+      $plot.attr('href', `#${movie.imdbID}`);
       $plot.text('Plot Synopsis');
 
       $action.append($plot);
       $card.append($action);
 
-      const $modal = $('<div>').addClass('modal').attr('id', movie.id);
+      const $modal = $('<div>').addClass('modal').attr('id', movie.imdbID);
       const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+      const $modalHeader = $('<h4>').text(movie.Title);
+      const $movieYear = $('<h6>').text(`Released in ${movie.Year}`);
+      const $modalText = $('<p>').text(movie.Plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
@@ -53,8 +55,28 @@
       $('#listings').append($col);
 
       $('.modal-trigger').leanModal();
+      $plot.click(function(){
+        $.get(`https://omdb-api.now.sh/?i=${movie.imdbID}`)
+        .done((data) => {
+          $modalText.append(data.Plot);
+        })
+      })
     }
   };
+  let searchBox = document.getElementById('search'),
+  searchButton = document.getElementById('searchButton');
 
-  // ADD YOUR CODE HERE
-})();
+    searchButton.addEventListener('click', function(e) {
+      if(searchBox.value != ''){
+        e.preventDefault();
+          $.get(`https://omdb-api.now.sh/?s=${searchBox.value}`)
+          .done((data) => {
+            movies = data.Search;
+            renderMovies();
+          })
+      } else {
+        e.preventDefault();
+        console.log('search a movie');
+      }
+    })
+})()
